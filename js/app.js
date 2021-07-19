3992adfe9855219d7981d4c8e99709c55d29169c
lokyle <lo7kyle@gmail.com>
2021-07-19 15:01:47 -0700

    updated index.html with table headers and filter container

--- js/app.js
+++ js/app.js
@@ -25,3 +25,27 @@ function buildTable(data) {
     });
 
 }
+// Function to filter table we built 
+function handleClick() {
+    // Tell D3 to look for the #datetime id in the HTML tags. 
+    // chaining select tells D3 to grab the info and hold it in the date variable
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+  buildTable(filteredData);
+}
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads (default)
+buildTable(tableData);
